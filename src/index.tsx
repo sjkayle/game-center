6bb266266fd916428e4d5bd556eedab69edbd64e
Kayle Copon <maxakyc@kayle-macbook-pro.realtek>
2021-09-12 12:25:45 +0800

    initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { store } from './redux/store';
+
+import './styles/main.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
