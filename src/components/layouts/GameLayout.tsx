177149660c165f0d01fecc3d3c4071895e3d59d0
Kayle Copon <maxakyc@kayle-macbook-pro.local>
2021-09-13 09:33:32 +0800

    remove redux selectors from layout component

--- src/components/layouts/GameLayout.tsx
+++ src/components/layouts/GameLayout.tsx
@@ -1,29 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
 import Navbar from '../Navbar';
-import { RootState } from '../../redux/store';
 
-const GameLayout = (props: GameLayoutProps) => {
-  const level = useSelector((state: RootState) => state.game.level);
-  const selectedGame = useSelector(
-    (state: RootState) => state.game.selectedGame
-  );
-  return (
-    <>
-      <Navbar />
-      <div className='wrapper'>
-        <h1>
-          {selectedGame}
-          {level && <span>, level {level}</span>}
-        </h1>
-        {props.children}
-      </div>
-    </>
-  );
-};
+const GameLayout = (props: GameLayoutProps) => (
+  <>
+    <Navbar />
+    <div className='wrapper'>
+      <h1>{props.heading}</h1>
+      {props.children}
+    </div>
+  </>
+);
 
 type GameLayoutProps = {
+  heading: string;
   children: any;
 };
 
